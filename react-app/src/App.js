f321fec1cab1c5c08b10f9b76f50babf3f0fe4f3
jjye <jjye81930@gmail.com>
2019-04-29 18:57:33 +0900

    data binding

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+class Subject extends Component{
+  render(){
+
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    var tags = [];
+    var con = this.props.data;
+    var i = 0;
+
+    while(i < con.length){
+      tags.push(<li key={con[i].id}><a href="">{con[i].title}</a></li>);
+      i = i + 1;
+    }
+
+    return (
+      <nav>
+        <ol>
+          {tags}
+        </ol>
+      </nav>
+    );
+  }
+}
+
+
+class Content extends Component{
+  render(){
+
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+
+
+
+class App extends Component{
+  state = {
+    contents:[
+      {id:1, title:"HTML", desc:"html is..."},
+      {id:1, title:"CSS", desc:"css is..."}
+    ]
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="World"></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <TOC data={this.state.contents}></TOC>
+        <TOC data={this.state.contents}></TOC>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is..."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
